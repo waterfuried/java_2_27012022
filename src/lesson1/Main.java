73069e602c933ba32aca3eda8c4ac556b2f67c85
waterfuried <cisternsystem@gmail.com>
2022-01-31 18:07:35 +0300

    Re-making new repository

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -14,4 +14,53 @@ package lesson1;
 	   то дальше по списку он препятствий не идет.
 */
 public class Main {
+    public static void main(String[] args) {
+        Cat leo = new Cat("Леопольд");
+        leo.setMaxRunDistance(400);
+
+        Movable[] participant = new Movable[] {
+            new Cat("Васька"),
+            new Robot(1),
+            leo,
+            new Human("Вася"),
+            new Human("Эдуард")
+        };
+
+        Obstacle[] obstacle = new Obstacle[] {
+            new Racetrack(300),
+            new Racetrack(-1),
+            null,
+            new Climbwall(5)
+        };
+
+        Movable[] curParticipant = new Movable[participant.length];
+        System.arraycopy(participant, 0, curParticipant, 0, participant.length);
+
+        for (Obstacle obs : obstacle) {
+            System.out.println("Препятствие: " +
+                    (obs == null || obs.getInfo() == null ? "неизвестный тип" : obs.getInfo()));
+            int i = 0;
+            while (i < curParticipant.length) {
+                int obstType = 0;
+                int obstSize = 0;
+                if (obs instanceof Racetrack) {
+                    obstSize = ((Racetrack) obs).length;
+                } else if (obs instanceof Climbwall) {
+                    obstType = 1;
+                    obstSize = ((Climbwall) obs).height;
+                } else {
+                    obstType = -1;
+                }
+                if (curParticipant[i] != null) {
+                    System.out.print("\tУчастник " + participant[i].getDesignation() + " ");
+                    if (!curParticipant[i].overcome(obstType, obstSize)) {
+                        System.out.print(" и выбывает");
+                        curParticipant[i] = null;
+                    }
+                    System.out.println();
+                }
+                i++;
+            }
+        }
+    }
 }
